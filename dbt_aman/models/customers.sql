e4a904953adfb2fac67f4b6f264f5c9fa1dea739
amanarora15 <107647568+amanarora15@users.noreply.github.com>
2023-07-05 18:35:31 +0000

    dbt 101

---
+++ dbt_aman/models/customers.sql
@@ -0,0 +1,24 @@
+{{ config(
+    materialized = 'table'
+)}}
+
+SELECT
+  a.id,
+  name,
+  email,
+  MIN(created_at) AS first_order_at,
+  COUNT(b.id) AS number_of_orders
+FROM
+  `analytics-engineers-club.coffee_shop.customers` a
+LEFT JOIN
+  `analytics-engineers-club.coffee_shop.orders` b
+ON
+  a.id = b.customer_id
+GROUP BY
+  1,
+  2,
+  3
+ORDER BY
+  first_order_at
+LIMIT
+  5
